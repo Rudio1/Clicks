61972ab0aa433269fdd875ae889747d34bdf6094
Guilherme Rudio <90154109+Rudio1@users.noreply.github.com>
2022-09-22 13:23:03 -0300

    Update Clicks.java

--- src/Clicks.java
+++ src/Clicks.java
@@ -7,82 +7,64 @@ import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
-
+import javafx.scene.control.Label;
 import java.util.concurrent.*;
 import static java.util.concurrent.TimeUnit.SECONDS;
 
-
-
 public class Clicks implements ActionListener {
-
-    
-
-    private int count = 0;
-    private JLabel label;
-    private JFrame frame;
-    private JPanel panel;
+    public int count = 0;
+    public JLabel label;
+    public JFrame frame;
+    public JPanel panel;
 
     public Clicks() {
         frame = new JFrame();
-
         JButton button = new JButton("Clicar");
         button.addActionListener(this);
-
         label = new JLabel("Clicks:");
-
         panel = new JPanel();
-        panel.setBorder(BorderFactory.createEmptyBorder(50, 50, 50, 15));
+        panel.setBorder(BorderFactory.createEmptyBorder(75, 75, 75, 100));
         panel.setLayout(new GridLayout(0,1));
         panel.add(button);
         panel.add(label);
-
         frame.add(panel, BorderLayout.CENTER);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setTitle("Contador de clicks");
         frame.pack();
         frame.setVisible(true);
     }
-
     public static void main(String[] args) {
-
-        Clicks clicks = new Clicks();
-       
+        Clicks clicks = new Clicks();  
         //Utilizando o timer para fazer uma contagem regressiva
         final ScheduledExecutorService tempo = Executors.newScheduledThreadPool(1);
         final Runnable runnable = new Runnable() {
-            int startCount = 5;
-            
-
+            int startCount = 5;       
             public void run() { 
                 System.out.println(startCount);
                 startCount--;
 
                 if(startCount < 0) {
-                    System.out.println("Acabou o tempo!!");
+                    System.out.println("Acabou o tempo!! \n" + clicks.toString() );
                     tempo.shutdown();
                     System.exit(0);
-                }
-                
+                } 
             }   
         }; 
-        tempo.scheduleAtFixedRate(runnable, 0, 1, SECONDS); 
-        
-          
-        
-
-        //Aqui eu quero mostrar a quantidade de clicks finais do usuario.
-        
+        tempo.scheduleAtFixedRate(runnable, 0, 1, SECONDS);     
     }
-
-
-    //Contabilizando a quantidade de clicks do usuario
-    @Override
-    public void actionPerformed(ActionEvent e) {
-        count++;
-        label.setText("clicks:" + count);
-    }
-
     public int getCount() {
         return count;
     }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        count++;
+        label.setText("clicks:" + getCount());
+    }
+
+    @Override
+    public String toString() {
+        return "A quantidade de clicks foi: " + getCount();
+    }
+
 }
